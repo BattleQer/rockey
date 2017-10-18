d83c30bd66ccdf1881882fbb9936180f96083e73
rockey <446175480@qq.com>
2017-10-18 19:26:40 +0800

    搜索

--- vue-project/src/containers/middle-content/index.vue
+++ vue-project/src/containers/middle-content/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <div class="form-search">
-            fl 筛选查询
+            fl 筛选查询  <div class="form-search-sty m-right-20 "><el-button type="text">展开筛选</el-button><el-button class="m-left-10">查询结果</el-button></div>
         </div>
         <div class="form-term ">
-            搜索选项 : <el-input size="large"  placeholder="请输入内容" > </el-input><el-input size="large"  placeholder="请输入内容" > </el-input><el-button class="m-left-10">默认按钮</el-button>
+            搜索选项 : <el-input size="large"  placeholder="请输入内容" style="width:400px;display:inline;margin-left:10px" > </el-input><el-input size="large"  placeholder="请输入内容" style="width:400px;display:inline;margin-left:10px"> </el-input><el-button class="m-left-10">默认按钮</el-button>
         </div>
         <router-view/>
     </div>
