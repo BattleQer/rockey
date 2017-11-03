0cf9f04ce8e3ed872c8098009d417c77f6620367
rockey <446175480@qq.com>
2017-11-03 10:42:27 +0800

    rockey

--- app-h5/h5Project/src/js/common/common.js
+++ app-h5/h5Project/src/js/common/common.js
@@ -2,16 +2,17 @@ require("../../css/common/common.css")
 import vue from "vue"
 import NativeShare from 'nativeshare'
 require("jqueryui")
+$("#radio").buttonset();
 var app = new vue({
     el: '#content',
     data: {
-   
+
     },
     mounted: function() {
- 
+
     },
     methods: {
-        ajaxq: function ajax(url, param, type,status,flag) {
+        ajaxq: function ajax(url, param, type, status, flag) {
             // 利用了jquery延迟对象回调的方式对ajax封装，使用done()，fail()，always()等方法进行链式回调操作
             // 如果需要的参数更多，比如有跨域dataType需要设置为'jsonp'等等，也可以不做这一层封装，还是根据工程实际情况判断吧，重要的还是链式回调
             // status 作为跨越和不跨域的处理 crossDomain 是否以文本流形式
@@ -19,13 +20,13 @@ var app = new vue({
                 url: url,
                 data: param || {},
                 type: type || 'GET',
-                dataType: status || "jsonp" ,
+                dataType: status || "jsonp",
                 jsonp: "callback",
                 crossDomain: true
             });
         },
-        handleAjax: function(url, param, type,status,flag) {
-            return this.ajaxq(url, param, type,status,flag).then(function(resp) {
+        handleAjax: function(url, param, type, status, flag) {
+            return this.ajaxq(url, param, type, status, flag).then(function(resp) {
                 // 成功回调 不作逻辑处理
                 if (resp) {
                     return resp; // 直接返回要处理的数据，作为默认参数传入之后done()方法的回调
@@ -37,8 +38,6 @@ var app = new vue({
                 console.log(err.status); // 打印状态码
             });
         },
-        
+
     }
 })
-
-$("#radio").buttonset();
