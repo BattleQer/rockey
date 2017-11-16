3bdb4db5a7de22797b940dfa1f9033540f71309b
rockey <446175480@qq.com>
2017-11-16 13:48:45 +0800

    rockey

--- app-h5/h5Project/build/webpack-config.js
+++ app-h5/h5Project/build/webpack-config.js
@@ -162,7 +162,7 @@ pagesM.forEach(function(pathname) {
         template: './src/html/music/' + htmlName + '.html', //html模板路径
         inject: 'body', //js插入的位置，true/'head'/'body'/false
         hash: true, //为静态资源生成hash值
-        chunks: ["vendor", "common", htmlName, ], //需要引入的chunk，不配置就会引入所有页面的资源
+        chunks: ["vendor", "common", htmlName, 'swiper'], //需要引入的chunk，不配置就会引入所有页面的资源
         minify: { //压缩HTML文件    
             removeComments: true, //移除HTML中的注释
             collapseWhitespace: false //删除空白符与换行符
